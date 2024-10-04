7acd214ae9ad7f4c8ad0bccde3e8c2fbf5acda34
Tyler Skaggs <tyler.skaggs314@gmail.com>
2024-10-03 19:05:05 -0500

    Added Richtmyer and MacCormack

--- Burgers.py
+++ Burgers.py
@@ -1,11 +1,48 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy import interpolate
-from numpy import where
-from matplotlib import animation
+from sympy.physics.units import length
+
 
 def f(x):
     return pow(x, 2) / 2
+
+
+def Richtmyer_Two_Step(k, h, init, xbound, tbound):
+    Nx = int((xbound[1] - xbound[0]) / h) + 1
+    Nt = int((tbound[1] - tbound[0]) / k) + 1
+
+    x = np.linspace(xbound[0], xbound[1], Nx)
+    y = init(x)
+    temp = init(x)
+    sol = np.zeros((Nx, Nt))
+    for t in range(0, Nt):
+        sol[:, t] = y
+        temp[1:-1] = 1 / 2 * ( y[1:-1] + y[2:] ) - k / (2 * h) * (f(y[2:]) - f(y[1:-1]))
+        y[1:-1] = y[1:-1] - k/h * ( f(temp[1:-1]) - f(temp[:-2]) )
+        #temp1 = 1 / 2 * ( y[1:-1] + y[2:] ) - k / (2 * h) * (f(y[2:]) - f(y[1:-1]))
+        #temp2 = 1 / 2 * ( y[:-2] + y[1:-1] ) - k / (2 * h) * (f(y[1:-1]) - f(y[:-2]))
+        #for i in range(1, Nx-1):
+            #y[i] = y[i] - k / h * (f(temp1[i-1]) - f(temp2[i-1]))
+
+    return sol
+
+
+def MacCormack(k, h, init, xbound, tbound):
+    Nx = int((xbound[1] - xbound[0]) / h) + 1
+    Nt = int((tbound[1] - tbound[0]) / k) + 1
+
+    x = np.linspace(xbound[0], xbound[1], Nx)
+    y = init(x)
+    temp = init(x)
+
+    sol = np.zeros((Nx, Nt))
+    for t in range(0, Nt):
+        sol[:, t] = y
+        temp[1:-1] = y[1:-1] - k / h * (f(y[1:-1]) - f(y[:-2]))
+
+        y[1:-1] = 1/2 * (y[1:-1] + temp[1:-1]) - k/(2*h) * (f(temp[1:-1]) - f(temp[:-2]))
+
+    return sol
 
 
 def LaxFriedrichs(k, h, init, xbound, tbound):
@@ -19,7 +56,6 @@ def LaxFriedrichs(k, h, init, xbound, tbound):
 
     for t in range(0, Nt):
         sol[:, t] = y
-
         y[1:-1] = 1/2 * (y[:-2] + y[2:]) - k / (2 * h) * (f(y[2:]) - f(y[:-2]))
 
     return sol
@@ -27,11 +63,13 @@ def LaxFriedrichs(k, h, init, xbound, tbound):
 
 def initial_condition1(x):
     y = np.zeros(np.size(x))
+    ul = 0
+    ur = 1
     for i in range(0, np.size(x)):
         if(x[i] >= 0.5):
-            y[i] = 0
+            y[i] = ur
         elif(x[i] < 0.5):
-            y[i] = 1
+            y[i] = ul
     return(y)
 
 
@@ -98,22 +136,30 @@ if __name__ == '__main__':
     x = np.linspace(xbounds[0], xbounds[1], Nx)  # discretization of space
     t = np.linspace(tbounds[0], tbounds[1], Nt)  # discretization of time
 
-    sol = LaxFriedrichs(k, h, initial_condition3, xbounds, tbounds)
+    sol1 = LaxFriedrichs(k, h, initial_condition3, xbounds, tbounds)
+    sol2 = Richtmyer_Two_Step(k, h, initial_condition3, xbounds, tbounds)
+    sol3 = MacCormack(k, h, initial_condition3, xbounds, tbounds)
 
     plt.ion()
     figure = plt.figure()
     axis = figure.add_subplot(111)
 
     line0, = axis.plot(x, initial_condition3(x), 'red', label='Analytical Solution')
-    line1, = axis.plot(x, sol[:, 0], 'blue', label='Numerical Solution')  # Returns a tuple of line objects, thus the comma
+    line1, = axis.plot(x, sol1[:, 0], 'blue', label='Lax-Wendroff')  # Returns a tuple of line objects, thus the comma
+    line2, = axis.plot(x, sol2[:, 0], 'green', label='Richtmyer')  # Returns a tuple of line objects, thus the comma
+    line3, = axis.plot(x, sol3[:, 0], 'orange', label='MacCormack')  # Returns a tuple of line objects, thus the comma
+
     plt.legend()
     plt.ylabel("u(x)")
     plt.xlabel("x")
     plt.title(r"Burgers' Equation")
+    plt.ylim(min(sol1[:, 0]) - 0.2, max(sol1[:,0]) + 0.2)
 
     for i in range(1, Nt):
         line0.set_ydata(analytical3(x, t[i]))
-        line1.set_ydata(sol[:, i])
+        line1.set_ydata(sol1[:, i])
+        line2.set_ydata(sol2[:, i])
+        line3.set_ydata(sol3[:, i])
         figure.canvas.draw()
         figure.canvas.flush_events()
 
