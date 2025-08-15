2cbec4b9a173b35e704c56eac5a7bb74e75686a4
Tyler Skaggs <tyler.skaggs314@gmail.com>
2025-08-15 15:31:13 -0500

    - Aug 15 Update

---
+++ DG.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+def burgers(x):
+    return pow(x, 2) / 2
+
+def DG():
+    return 0
+
+if __name__ == '__main__':
+    print("HELLO")
---
+++ Problem1.py
@@ -0,0 +1,93 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from numpy import where
+from scipy.stats import linregress
+
+
+def init(x, lam):
+    return 1/(1 + np.exp(lam * x))
+
+
+if __name__ == '__main__':
+
+    dx = 1 / 20
+    dt = dx / 2
+
+    a = -10
+    b = 50
+    time = 10
+
+    k = [1, 1.2, 2.5, 3]
+    lam = [0.49, 0.45, 0.34, 0.8]
+
+    Nx = int((b - a) / dx) + 1
+    Nt = int(time / dt) + 1
+
+    x = np.linspace(a, b, Nx)
+    t = np.linspace(0, time, Nt)
+
+    plt.ion()
+    figure = plt.figure()
+    axis = figure.add_subplot(111)
+
+    U = [init(x, lam[0]), init(x, lam[1]), init(x, lam[2]), init(x, lam[3])]
+
+    lineSOL0, = axis.plot(x, U[0], color='red', label=r'$k = 1, \ \lambda = 0.49$')
+    lineSOL1, = axis.plot(x, U[1], color='blue', label=r'$k = 1.2, \ \lambda = 0.45$')
+    lineSOL2, = axis.plot(x, U[2], color='green', label=r'$k = 2.5, \ \lambda = 0.34$')
+    lineSOL3, = axis.plot(x, U[3], color='orange', label=r'$k = 3, \ \lambda = 0.8$')
+
+    plt.ylim(-0.5, 2.5)
+    #plt.xlim(-10, 50)
+    plt.legend()
+    plt.xlabel("x")
+    plt.ylabel("u(x,t)")
+    plt.title(r'Numerical Solution to $u_t - ku_{xxt} = u_{xx} +u(1-u), \quad \Delta x = \frac{1}{20}, \ \frac{\Delta t}{\Delta x} = \frac{1}{2}$')
+
+    text = plt.text(37.5, 1.5, "t = 0")
+
+    Dxx = -2 * np.eye(Nx)
+    for i in range(0, Nx - 1):
+        Dxx[i, i + 1] = Dxx[i + 1, i] = 1
+
+    # Forward Difference for Left Endpoint
+    Dxx[0,0] = 1
+    Dxx[0,1] = -2
+    Dxx[0,2] = 1
+
+    # Backward Difference for Right Endpoint
+    Dxx[-1, -1] = 1
+    Dxx[-1, -2] = -2
+    Dxx[-1, -3] = 1
+
+    Dxx *= np.power(dx,-2)
+
+    M = [np.eye(Nx) - k[0] * Dxx, np.eye(Nx) - k[1] * Dxx, np.eye(Nx) - k[2] * Dxx, np.eye(Nx) - k[3] * Dxx]
+    Minv = [np.linalg.inv(M[0]), np.linalg.inv(M[1]), np.linalg.inv(M[2]), np.linalg.inv(M[3])]
+
+    t = 0
+    i=0
+    while t < time-dt/2:
+        for j in range(4):
+            V = M[j] @ U[j]
+            V += dt * (-1 / k[j] * V + (1 / k[j] + 1) * U[j] - np.power(U[j], 2))  # update V
+
+            U[j] = Minv[j] @ V # Update U
+
+
+        t += dt
+        text.set_text("t = %f" % t)
+
+        lineSOL0.set_ydata(U[0])
+        lineSOL1.set_ydata(U[1])
+        lineSOL2.set_ydata(U[2])
+        lineSOL3.set_ydata(U[3])
+
+        figure.canvas.draw()
+        figure.canvas.flush_events()
+        i+=1
+        figure.savefig('C:/Users/tyler/Documents/Plots/figure' + str(i) + '.png')
+
+
+    plt.ioff()
+    plt.show()
