fd98738d15b52e62bf56bc3894fb95acefb1a62d
Tyler Skaggs <tyler.skaggs314@gmail.com>
2025-09-02 21:10:46 -0500

    - DGRK update 2, minmod and u3 fix

--- DG.py
+++ DG.py
@@ -25,7 +25,7 @@ def v(x, j, l, xarr, dx): #Legendre Polynomials
     else:
         return np.power(x - xarr[j], 2) - 1/12 * np.power(dx, 2)
 
-def DG(x, u0, u1, u2, dx, dt, problem, deriv):
+def DG(x, u0, u1, u2, dx, dt, problem, deriv, order=3):
     def hRF(a, b):  # Roe Flux with entropy fix
         low = min(a, b)
         high = max(a, b)
@@ -45,9 +45,9 @@ def DG(x, u0, u1, u2, dx, dt, problem, deriv):
             beta = np.abs(deriv(optimize.minimize_scalar(lambda z: -np.abs(deriv(z)), bounds=[low, high], method='bounded').x))
             return 1 / 2 * (problem(a) + problem(b) - beta * (b - a))
 
-    def mm(arr): # modified minmod function
-        M2 = 2/3
-        M = 1 * M2
+    def mm(arr, j): # modified minmod function
+        M2 = 0
+        M = 2/9 * (3 + 10 * M2) * M2 * (np.power(dx, 2) / (np.power(dx,2) + np.abs(u0[j+1] - u0[j]) + np.abs(u0[j] - u0[j-1])) )
         def m(arr): # minmod function
             if sum(arr < 0) == 0:
                 return min(abs(arr))
@@ -62,21 +62,27 @@ def DG(x, u0, u1, u2, dx, dt, problem, deriv):
             return m(arr)
 
     def L(u0, u1, u2):
+        left = 2
+        right = 0
         for i in range(2):  # left bound
-            u0[i] = u0[-3 + i]
-            u1[i] = u1[-3 + i]
-            u2[i] = u2[-3 + i]
+            u0[i] = left #u0[-3 + i]
+            u1[i] = left #u1[-3 + i]
+            u2[i] = left #u2[-3 + i]
 
         for i in range(2):  # right bound
-            u0[-2 + i] = u0[1 + i]
-            u1[-2 + i] = u1[1 + i]
-            u2[-2 + i] = u2[1 + i]
+            u0[-2 + i] = right #u0[1 + i]
+            u1[-2 + i] = right #u1[1 + i]
+            u2[-2 + i] = right #u2[1 + i]
 
         n = len(u0)
         L0, L1, L2 = np.zeros(n), np.zeros(n), np.zeros(n)
 
-        utilde = 6*u1 + 30*u2
-        uttilde = 6*u1 - 30*u2
+        if order == 3:
+            utilde = 6*u1 + 30*u2
+            uttilde = 6*u1 - 30*u2
+        else:
+            utilde = 6*u1
+            uttilde = 6*u1
 
         utilde_mod = utilde
         uttilde_mod = uttilde
@@ -85,8 +91,8 @@ def DG(x, u0, u1, u2, dx, dt, problem, deriv):
         uplus_mod = np.zeros(n)
 
         for j in range(1, n-1):
-            utilde_mod[j] = mm(np.array([utilde[j], u0[j+1] - u0[j], u0[j] - u0[j-1]]))
-            uttilde_mod[j] = mm(np.array([uttilde[j], u0[j+1] - u0[j], u0[j] - u0[j-1]]))
+            utilde_mod[j] = mm(np.array([utilde[j], u0[j+1] - u0[j], u0[j] - u0[j-1]]), j)
+            uttilde_mod[j] = mm(np.array([uttilde[j], u0[j+1] - u0[j], u0[j] - u0[j-1]]), j)
 
             uminus_mod[j] = u0[j] + utilde_mod[j]
             uplus_mod[j-1] = u0[j] - uttilde_mod[j]
@@ -110,8 +116,11 @@ def DG(x, u0, u1, u2, dx, dt, problem, deriv):
                 j = j - 1
 
             a0 = a[0] * u0[j]
-            a1 = a[1] * u1[j] * (val - x[j])
-            a2 = a[2] * u2[j] * (np.power(val - x[j], 2) - 1/12 * np.power(dx, 2) )
+            a1 = a[1] * u1mod[j] * (val - x[j])
+            if order == 3:
+                a2 = a[2] * u2mod[j] * (np.power(val - x[j], 2) - 1/12 * np.power(dx, 2) )
+            else:
+                a2 = 0
             return  a0 + a1 + a2
 
         for j in range(1, n-1):
@@ -129,10 +138,6 @@ def DG(x, u0, u1, u2, dx, dt, problem, deriv):
                              sum(vals)
                              )#Gauss-Lobotto 6 point evaluation
 
-            """GLint1 = dx/180 * (9*(hPlusHalf[j] + hPlusHalf[j-1]) +
-                               64*problem(u0[j]) +
-                               49*problem(U(dx/2 * np.sqrt(3/7) + x[j])) +
-                               49*problem(U(-dx/2 * np.sqrt(3/7) + x[j]))) #Gauss-Lobotto 5 point evaluation"""
 
             L1[j] = -1/(2 * dx) * (hPlusHalf[j] + hPlusHalf[j-1]) + 1/dx * GLint1
 
@@ -144,43 +149,32 @@ def DG(x, u0, u1, u2, dx, dt, problem, deriv):
                       vals[2] * (dx / 2 * np.sqrt(1 / 3 - 2 * np.sqrt(7) / 21)) +
                       vals[3] * (-dx / 2 * np.sqrt(1 / 3 - 2 * np.sqrt(7) / 21))
                       )
-            """GLint2 = 1/90 * (9*(hPlusHalf[j] * dx/2 + hPlusHalf[j-1]*-dx/2) +
-                               49*problem(U(dx/2 * np.sqrt(3/7) + x[j]) * (dx/2 * np.sqrt(3/7)) +
-                               49*problem(U(-dx/2 * np.sqrt(3/7) + x[j])) * (-dx/2 * np.sqrt(3/7)))) #Gauss-Lobotto 5 point evaluation"""
 
-            L2[j] = -1/(6 * dx) *  (hPlusHalf[j] - hPlusHalf[j-1]) + 1/(np.power(dx, 3)) * GLint2
+
+            L2[j] = -1/(6 * dx) *  (hPlusHalf[j] - hPlusHalf[j-1]) + 1/(np.power(dx, 2)) * GLint2
 
         return L0, L1, L2
 
-    alpha0 = [1, 1/2, 1/9, 0]
-    alpha1 = [0, 1/2, 2/9, 1/3]
-    alpha2 = [0, 0, 2/3, 1/3]
-    alpha3 = [0, 0, 0, 1/3]
-    alphaL0 = [1/2, -1/4, -1/9, 0]
-    alphaL1 = [0, 1/2, -1/3, 1/6]
-    alphaL2 = [0, 0, 1, 0]
-    alphaL3 = [0, 0, 0, 1/6]
+    uk = np.array([[u0.copy(), u0.copy(), u0.copy(), u0.copy()],
+                   [u1.copy(), u1.copy(), u1.copy(), u1.copy()],
+                   [u2.copy(), u2.copy(), u2.copy(), u2.copy()]])
 
-    uk = np.array([[u0.copy(), u0.copy(), u0.copy(), u0.copy(), u0.copy()],
-                   [u1.copy(), u1.copy(), u1.copy(), u1.copy(), u0.copy()],
-                   [u2.copy(), u2.copy(), u2.copy(), u2.copy(), u0.copy()]])
+    Lk = L(uk[0, 0], uk[1, 0], uk[2, 0])
+    for l in range(3):
+        uk[l, 1][1:-1] = uk[l,0][1:-1] + dt * Lk[l][1:-1]
 
-    Lk = np.array([[u0.copy(), u0.copy(), u0.copy(), u0.copy()],
-                   [u0.copy(), u0.copy(), u0.copy(), u0.copy()],
-                   [u0.copy(), u0.copy(), u0.copy(), u0.copy()]])
+    Lk = L(uk[0, 1], uk[1, 1], uk[2, 1])
+    for l in range(3):
+        uk[l, 2][1:-1] = 3/4 * uk[l,0][1:-1] + 1/4*uk[l, 1][1:-1] + dt/4 * Lk[l][1:-1]
 
-    for k in range(4):
-        Lk[:,k] = L(uk[0, k], uk[1, k], uk[2, k])
+    Lk = L(uk[0, 2], uk[1, 2], uk[2, 2])
+    for l in range(3):
+        uk[l, 3][1:-1] = 1/3 * uk[l,0][1:-1] + 2/3*uk[l,2][1:-1] + 2/3*dt*Lk[l][1:-1]
+
+
+    for i in range(2):
         for l in range(3):
-            uk[l, k + 1][1:-1] = \
-                (alpha0[k] * uk[l, 0][1:-1] +
-                 alpha1[k] * uk[l, 1][1:-1] +
-                 alpha2[k] * uk[l, 2][1:-1] +
-                 alpha3[k] * uk[l, 3][1:-1] +
-                 alphaL0[k] * dt * Lk[l, 0][1:-1] +
-                 alphaL1[k] * dt * Lk[l, 1][1:-1] +
-                 alphaL2[k] * dt * Lk[l, 2][1:-1] +
-                 alphaL3[k] * dt * Lk[l, 3][1:-1])
+            uk[l, -1][i] = 2 #uk[l, -1][-3 + i]
 
     return uk[0, -1], uk[1, -1], uk[2, -1]
 
@@ -230,22 +224,24 @@ def analytical3(x, t):
 
 if __name__ == '__main__':
     def init(x):
-        return 1/4+1/2*np.sin(np.pi * x)
-        #return analytical3(x, 0)
+        #return 1/4+1/2*np.sin(np.pi * x)
+        return analytical3(x, 0)
 
     def analytic(x,t):
-        return init(x - t)
-        #return analytical3(x,t)
+        #return init(x - t)
+        return analytical3(x,t)
 
+    order = 3
+    plot = 1
 
-    problem = burgers_prime
-    deriv = linear_deriv
+    problem = burgers
+    deriv = burgers_prime
 
-    dx = 1/40
-    dt = dx/3 #np.power(dx, 5/4)
+    dx = 1/20
+    dt = dx/4 #np.power(dx, 5/4)
 
     a = -1
-    b = 1
+    b = 4
     time = 2
 
     Nx = int((b-a) / dx) + 1
@@ -254,11 +250,9 @@ if __name__ == '__main__':
     x = np.linspace(a-dx, b+dx, Nx+2)
     t = np.linspace(0, time, Nt)
 
-    plot = 1
 
     if plot == 1:
-        xx = np.linspace(-1, 1, 100)
-        DGSOL = init(x)
+        DGSOL = x.copy()
 
         plt.ion()
         figure = plt.figure()
@@ -266,10 +260,10 @@ if __name__ == '__main__':
 
         line0, = axis.plot(x[1:-1], init(x[1:-1]), 'red', label='Analytical Solution')
         lineDG, = axis.plot(x[1:-1], init(x[1:-1]), color='purple', label='DGRK Solution')
-        #lineSOL, = axis.plot(x[1:-1], init(x[1:-1]), color='blue', label='DGRK Solution')
+        #lineSOL, = axis.plot(x[1:-1], init(x[1:-1]), color='blue', label='DGRK Func Solution')
 
 
-        plt.ylim(-0.3, 0.8)
+        plt.ylim(-0.3, 2.3)
         plt.legend()
         plt.xlabel("x")
         plt.ylabel("u(x,t)")
@@ -290,12 +284,12 @@ if __name__ == '__main__':
 
             u0[j] = 1/dx * integrate.fixed_quad(init, lowbound, highbound, n = 6)[0]
 
-            u1[j] = 1/np.power(dx,2) * integrate.fixed_quad(lambda z: init(z)*v(z, j, 1, x, dx), lowbound, highbound, n = 6)[0]
+            u1[j] = 1/np.power(dx,2) * integrate.fixed_quad(lambda z: init(z)*(z - x[j]), lowbound, highbound, n = 6)[0]
 
-            u2[j] = 1/np.power(dx,3) * integrate.fixed_quad(lambda z: init(z)*v(z, j, 2, x, dx), lowbound, highbound, n = 6)[0]
+            u2[j] = 1/np.power(dx,3) * integrate.fixed_quad(lambda z: init(z)*(np.power(z-x[j], 2) - 1/12 * np.power(dx, 2)), lowbound, highbound, n = 6)[0]
 
 
-        def U(val, u0_, u1_, u2_):
+        def Uu(val, u0_, u1_, u2_):
             a = [1, 12 / dx, 180 / np.power(dx, 2)]
             j = sum(x <= val)
             if not (x[j] + dx / 2 >= val >= x[j] - dx / 2):
@@ -303,14 +297,17 @@ if __name__ == '__main__':
 
             a0 = a[0] * u0_[j]
             a1 = a[1] * u1_[j] * (val-x[j])
-            a2 = a[2] * u2_[j] * (np.power(val - x[j], 2) - 1 / 12 * np.power(dx, 2))
+            if order == 3:
+                a2 = a[2] * u2_[j] * (np.power(val - x[j], 2) - 1 / 12 * np.power(dx, 2) )
+            else:
+                a2 = 0
             return a0 + a1 + a2
 
-        while t < time-dt/2:
-            u0, u1, u2 = DG(x, u0, u1, u2, dx, dt, problem, deriv)
+        while t < time - dt/2:
+            u0, u1, u2 = DG(x, u0, u1, u2, dx, dt, problem, deriv, order)
 
             for j in range(0, len(x) - 1):
-                DGSOL[j] = U(x[j], u0, u1, u2)
+                DGSOL[j] = Uu(x[j], u0, u1, u2)
 
             t += dt
             text.set_text("t = %f" % t)
@@ -331,14 +328,14 @@ if __name__ == '__main__':
         print("N = ", len(x), "\n\tL1 Error: ", error1, '\n\tLinf Error: ', errorinf)"""
 
     else:
-        Nvals = np.array([20, 40, 80, 160])
+        Nvals = np.array([5, 10, 20, 40, 80, 160])
         hvals = (b - a) / (Nvals - 1)
         hs = -1
-        maxerror_EWENO = np.zeros((len(hvals), 3))
+        maxerror_DG = np.zeros((len(hvals), 3))
 
         for dx in hvals:
             hs = hs + 1
-            dt = dx / 3
+            dt = dx / 4
 
             Nx = Nvals[hs]
             Nt = int(time / dt) + 1
@@ -346,9 +343,9 @@ if __name__ == '__main__':
             x = np.linspace(a - dx, b + dx, Nx + 2)
             t = np.linspace(0, time, Nt)
 
-            error_1_EWENO = np.zeros(len(t))
-            error_2_EWENO = np.zeros(len(t))
-            error_inf_EWENO = np.zeros(len(t))
+            error_1_DG = np.zeros(len(t))
+            error_2_DG = np.zeros(len(t))
+            error_inf_DG = np.zeros(len(t))
 
             t = 0
             i = 0
@@ -364,25 +361,46 @@ if __name__ == '__main__':
                 u0[j] = 1 / dx * integrate.fixed_quad(init, lowbound, highbound, n=6)[0]
 
                 u1[j] = 1 / np.power(dx, 2) * \
-                        integrate.fixed_quad(lambda z: init(z) * v(z, j, 1, x, dx), lowbound, highbound, n=6)[0]
+                        integrate.fixed_quad(lambda z: init(z) * (z - x[j]), lowbound, highbound, n=6)[0]
 
                 u2[j] = 1 / np.power(dx, 3) * \
-                        integrate.fixed_quad(lambda z: init(z) * v(z, j, 2, x, dx), lowbound, highbound, n=6)[0]
+                        integrate.fixed_quad(lambda z: init(z) * (np.power(z - x[j], 2) - 1 / 12 * np.power(dx, 2)),
+                                             lowbound, highbound, n=6)[0]
+
+            DGSOL = x.copy()
+            def Uu(val, u0_, u1_, u2_):
+                a = [1, 12 / dx, 180 / np.power(dx, 2)]
+                j = sum(x <= val)
+                if not (x[j] + dx / 2 >= val >= x[j] - dx / 2):
+                    j = j - 1
+
+                a0 = a[0] * u0_[j]
+                a1 = a[1] * u1_[j] * (val - x[j])
+                if order == 3:
+                    a2 = a[2] * u2_[j] * (np.power(val - x[j], 2) - 1 / 12 * np.power(dx, 2))
+                else:
+                    a2 = 0
+
+                return a0 + a1 + a2
 
             while t < time-dt/2:
-                u0, u1, u2 = DG(x, u0, u1, u2, dx, dt, problem, deriv)
+                u0, u1, u2 = DG(x, u0, u1, u2, dx, dt, problem, deriv, order)
                 t += dt
 
+            for j in range(0, len(x) - 1):
+                DGSOL[j] = Uu(x[j], u0, u1, u2)
+
             plt.plot(x[1:-1], analytic(x[1:-1], t), color='red', label="Analytic")
-            plt.plot(x[1:-1], u0[1:-1], color='purple', label='E-WENO')
+            plt.plot(x[1:-1], DGSOL[1:-1], color='purple', label='DG Func Solution')
+            plt.plot(x[1:-1], u0[1:-1], color='purple', label='DG Solution')
             plt.legend()
             plt.show()
 
-            tempEWENO = abs(analytic(x[1:-1], t) - u0[1:-1])
+            tempError = abs(analytic(x[1:-1], t) - u0[1:-1])
 
-            maxerror_EWENO[hs, 0] = dx * sum(tempEWENO)
-            maxerror_EWENO[hs, 1] = pow(dx * sum(pow(tempEWENO, 2)), 1 / 2)
-            maxerror_EWENO[hs, 2] = max(tempEWENO)
+            maxerror_DG[hs, 0] = dx * sum(tempError)
+            maxerror_DG[hs, 1] = pow(dx * sum(pow(tempError, 2)), 1 / 2)
+            maxerror_DG[hs, 2] = max(tempError)
 
         logH = np.log2(hvals)
         figure, axis = plt.subplots(2, 2)
@@ -391,9 +409,9 @@ if __name__ == '__main__':
             figplace1 = (0, 0, 1, 1)
             figplace2 = (0, 1, 0, 1)
 
-            slope_EWENO = linregress(logH, np.log2(maxerror_EWENO[:, i]))[0]
+            slope_EWENO = linregress(logH, np.log2(maxerror_DG[:, i]))[0]
 
-            axis[figplace1[i], figplace2[i]].plot(-logH, -np.log2(maxerror_EWENO[:, i]),
+            axis[figplace1[i], figplace2[i]].plot(-logH, -np.log2(maxerror_DG[:, i]),
                                                   label="EWENO_Error - Slope %f" % slope_EWENO, color="purple")
 
             axis[figplace1[i], figplace2[i]].legend()
@@ -404,8 +422,8 @@ if __name__ == '__main__':
         plt.show()
 
         print('N    ', 'L_inf                     ', 'Ord                         ', 'L1                        ', 'Ord')
-        print(f"{Nvals[0]:3d}", ' ', f"{maxerror_EWENO[0, 2]:3.22f}", '                            | ', f"{maxerror_EWENO[0, 0]:3.22f}")
+        print(f"{Nvals[0]:3d}", ' ', f"{maxerror_DG[0, 2]:3.22f}", '                            | ', f"{maxerror_DG[0, 0]:3.22f}")
         for i in range(1, len(Nvals)):
-            ord_inf = (-np.log2(maxerror_EWENO[i, 2]) + np.log2(maxerror_EWENO[i - 1, 2])) / (-logH[i] + logH[i - 1])
-            ord_1 = (-np.log2(maxerror_EWENO[i, 0]) + np.log2(maxerror_EWENO[i - 1, 0])) / (-logH[i] + logH[i - 1])
-            print(f"{Nvals[i]:3d}", ' ', f"{maxerror_EWENO[i, 2]:3.22f}", ' ', f"{ord_inf:3.22f}", ' | ', f"{maxerror_EWENO[i, 0]:3.22f}", ' ', f"{ord_1:3.22f}")
+            ord_inf = (-np.log2(maxerror_DG[i, 2]) + np.log2(maxerror_DG[i - 1, 2])) / (-logH[i] + logH[i - 1])
+            ord_1 = (-np.log2(maxerror_DG[i, 0]) + np.log2(maxerror_DG[i - 1, 0])) / (-logH[i] + logH[i - 1])
+            print(f"{Nvals[i]:3d}", ' ', f"{maxerror_DG[i, 2]:3.22f}", ' ', f"{ord_inf:3.22f}", ' | ', f"{maxerror_DG[i, 0]:3.22f}", ' ', f"{ord_1:3.22f}")
